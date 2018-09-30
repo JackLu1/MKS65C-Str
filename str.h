b52c2509dcf2cba1c1ef82906b7384709f478da7
jack <jlu2025@gmail.com>
2018-09-30 18:24:47 -0400

    working mystrcat

--- str.h
+++ str.h
@@ -1,6 +1,6 @@
 int mystrlen( char * s); //done
 char * mystrncpy( char *dest, char *source );
-char * mystrcat( char *dest, char *source );
+char * mystrcat( char *dest, char *source ); //done
 int mystrcmp( char *s1, char *s2 );
 char * mystrchr( char *s, char c );
 
