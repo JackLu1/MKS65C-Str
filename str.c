43f427686fc9482eef324b5ac9a8a3f7a9bd0d6d
nadinejackson <njackson@stuy.edu>
2018-09-30 21:10:24 -0400

    added comments

--- str.c
+++ str.c
@@ -3,35 +3,35 @@
 
 int mystrlen( char * s){
   int a = 0;
-  while(*s++){a++;}
-  return a;
+  while(*s++){a++;} //iterates to null char, 
+  return a; //returns how many chars you went through
 }
 char * mystrcat( char *dest, char *source ){
   char * ans = dest;
   while(*dest){dest++;} //iterate to terminating 0
-  while(*dest++ = *source++);
-  return ans;
+  while(*dest++ = *source++); //concatonate until and including null
+  return ans; //pointer to char array
 }
 char * mystrncpy( char *dest, char *source, int n )
 {
-  while (*source && n--)
+  while (*source && n--) //until ending null or n characters, whichever is first
     {
-      *dest++ = *source++;
+      *dest++ = *source++; //copy character over
     }
-  *dest = 0;
+  *dest = 0; //add null
 }
 int mystrcmp( char *s1, char *s2 )
 {
-  while (*s1 && *s2 && *s1 == *s2){
-    s1++;
+  while (*s1 && *s2 && *s1 == *s2){ //until reach end or difference
+    s1++;//keep going 
     s2++;
   }
-  return *s1 - *s2;
+  return *s1 - *s2; //0 if equal, - if s1 is less, + if s1 is greater
 }
 char * mystrchr( char *s, char c )
 {
-  while (*s != c)
-    if (!*s++)
-	return 0;
-  return s;
+  while (*s != c) //until you find the character
+    if (!*s++) //if you reach end of string
+      return 0; //return nil address
+  return s; //location of c
 }
